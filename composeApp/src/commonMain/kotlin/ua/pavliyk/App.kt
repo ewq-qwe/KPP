7870d7031a8fcd988ce2d5bd66fd07d9e2d1fc58
ewq-qwe <pavliuk.mykyta@chnu.edu.ua>
2026-04-15 17:33:04 +0300

    4 лаба (timezones on mobile)

--- composeApp/src/commonMain/kotlin/ua/pavliyk/App.kt
+++ composeApp/src/commonMain/kotlin/ua/pavliyk/App.kt
@@ -5,12 +5,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import ua.pavliyk.data.timezones.TimeZoneHelperImpl
 import ua.pavliyk.ui.screens.AppNavigation
+import ua.pavliyk.ui.shared_mobile.main.MainScreen
 import ua.pavliyk.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        AppNavigation()
+        MainScreen()
              }
         }
---
+++ composeApp/src/commonMain/kotlin/ua/pavliyk/ui/types/Types.kt
@@ -0,0 +1,7 @@
+package ua.pavliyk.ui.types
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EmptyComposable() {
+}
