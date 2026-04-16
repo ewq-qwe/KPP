ebb97294f8eda6d5ce02b90e9df09a1ead2c158a
ewq-qwe <pavliuk.mykyta@chnu.edu.ua>
2026-04-16 23:37:48 +0300

    5 лаба (timezones on desktop and web)

--- composeApp/src/commonMain/kotlin/ua/pavliyk/ui/types/Types.kt
+++ composeApp/src/commonMain/kotlin/ua/pavliyk/ui/types/Types.kt
@@ -2,6 +2,13 @@ package ua.pavliyk.ui.types
 
 import androidx.compose.runtime.Composable
 
+typealias onAddType =  (List<String>) -> Unit
+
+typealias onDismissType =  () -> Unit
+
+typealias composeFun =  @Composable () -> Unit
+
+typealias topBarFun =  @Composable (Int) -> Unit
 @Composable
 fun EmptyComposable() {
 }
